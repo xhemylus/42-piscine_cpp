dd5445732676ff63d4375be6dff0ebf24e69f383
Aurelien Bollen <abollen@z4r4p5.42lyon.fr>
2022-05-17 14:55:07 +0200

    Finished Megaphone.

---
+++ Module_00/ex00/megaphone.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int	main(int argc, char** argv)
+{
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		int i = 0;
+		while (++i < argc)
+		{
+			std::string str(argv[i]);
+			std::transform(str.begin(), str.end(), str.begin(), ::toupper);
+			std::cout << str;
+		}
+		std::cout << std::endl;
+	}
+	return 0;
+}
