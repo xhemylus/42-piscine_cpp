a5d0f2f393620d08f40e27b0196536550868a01a
Lézarovitch <lezarovitch@protonmail.com>
2023-01-20 12:57:53 +0100

    updated main

--- Module_08/ex02/src/main.cpp
+++ Module_08/ex02/src/main.cpp
@@ -11,7 +11,8 @@ int main()
 	std::cout << mstack.size() << std::endl;
 	mstack.push(3);
 	mstack.push(5);
-	mstack.push(737); //[...] mstack.push(0);
+	mstack.push(737);
+	mstack.push(0);
 	MutantStack<int>::iterator it = mstack.begin();
 	MutantStack<int>::iterator ite = mstack.end();
 	++it;
